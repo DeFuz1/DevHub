7088cfc836c806041b60a83c0779624f3216154b
DeFuz1 <webweb937@gmail.com>
2026-08-30 22:42:57 +0300

    add minimalsize

--- desktopApp/src/main/kotlin/com/example/devhub/main.kt
+++ desktopApp/src/main/kotlin/com/example/devhub/main.kt
@@ -1,15 +1,24 @@
 package com.example.devhub
 
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 
 import com.example.devhub.ui.components.screens.MainWorkspaceScreen
+import java.awt.Dimension
 
 fun main() = application {
+    val windowState = rememberWindowState(width = 1400.dp, height = 800.dp)
     Window(
         onCloseRequest = ::exitApplication,
+        state = windowState,
         title = "DevHub Workspace"
     ) {
+        LaunchedEffect(Unit) {
+            window.minimumSize = Dimension(1400, 800)
+        }
         MainWorkspaceScreen()
     }
 }
